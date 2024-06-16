0e52ecf7b7eb7464714c00f1438beeff29696f99
Warren Zhao <warrenzhao2016@gmail.com>
2024-06-16 14:58:38 +0800

    no.128 explain

--- no_128_valid_palindrome.py
+++ no_128_valid_palindrome.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # 大写字母转小写
-        low_s = s.lower()
+        low_s = s.lower()  # 仅仅调用一次lower()函数，执行时间更短，使用内存更少
         # 排除所有的符号
         new_s = "".join(ch for ch in low_s if ch.isalnum())
         # 验证是否是回文
