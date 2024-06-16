b4215dc4e42d169d08cda90096fea56926240da8
Warren Zhao <warrenzhao2016@gmail.com>
2024-06-16 15:39:28 +0800

    no.383

---
+++ no_383_ransomnote.py
@@ -0,0 +1,24 @@
+import collections
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        return not collections.Counter(magazine) - collections.Counter(ransomNote)
+
+    def canConstruct_selfcode(self, ransomNote: str, magazine: str) -> bool:
+        ch_dict = dict()
+        for ch in magazine:
+            if ch in ch_dict:
+                ch_dict[ch] += 1
+            else:
+                ch_dict[ch] = 1
+        for ch in ransomNote:
+            if ch not in ch_dict:
+                return False
+            else:
+                ch_dict[ch] -= 1
+                if ch_dict[ch] == -1:
+                    return False
+        return True
+        
+s=Solution()
+print(not s.canConstruct("aab", "aab"))
+                
